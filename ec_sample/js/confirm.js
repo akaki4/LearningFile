34654486abd352492db567899c64afd2fd403a62
takumi <akaranki4@gmail.com>
2021-01-17 15:07:23 +0900

    検索機能追加

--- ec_sample/js/confirm.js
+++ ec_sample/js/confirm.js
@@ -1,20 +1,20 @@
 window.onload = function () {
-  var items = JSON.parse(localStorage.getItem("items")),//ローカルストレージの商品データの配列
-  ele = document.getElementById('js_shopping_list'),//カートの商品を追加する要素
-  fragment = document.createDocumentFragment(),//DOMの追加処理用のフラグメント
-  total = 0,//商品の合計金額
-  total_ele = document.getElementById('js_total'),//商品の合計金額表示用の要素
-  confirm_btn = document.getElementById('js_confirm');//購入確定ボタン
+  var items = JSON.parse(localStorage.getItem("items")), //ローカルストレージの商品データの配列
+    ele = document.getElementById("js_shopping_list"), //カートの商品を追加する要素
+    fragment = document.createDocumentFragment(), //DOMの追加処理用のフラグメント
+    total = 0, //商品の合計金額
+    total_ele = document.getElementById("js_total"), //商品の合計金額表示用の要素
+    confirm_btn = document.getElementById("js_confirm"); //購入確定ボタン
 
   if (items) {
     // カート商品の数分、要素を作成
     for (var i = 0; i < items.length; i++) {
-      var li = document.createElement('li'),
-      h2 = document.createElement('h2'),
-      price = document.createElement('div');
+      var li = document.createElement("li"),
+        h2 = document.createElement("h2"),
+        price = document.createElement("div");
 
-      price.classList.add('price');
-      
+      price.classList.add("price");
+
       h2.appendChild(document.createTextNode(items[i].name));
       price.appendChild(document.createTextNode(items[i].price));
       li.appendChild(h2);
@@ -31,9 +31,8 @@ window.onload = function () {
   // 合計金額の表示
   total_ele.innerHTML = total;
 
-  confirm_btn.addEventListener('click',function () {
-    window.alert('購入が確定しました。');
-    localStorage.removeItem('items');
+  confirm_btn.addEventListener("click", function () {
+    window.alert("購入が確定しました。");
+    localStorage.removeItem("items");
   });
-
 };
--- ec_sample/js/script.js
+++ ec_sample/js/script.js
@@ -1,85 +1,125 @@
 window.onload = function () {
-  var cart_btns = document.querySelectorAll('.js_cart_btn'),//カートボタン
-  cart_cnt_icon = document.getElementById('js_cart_cnt'),//カートの個数アイコン
-  cart_cnt = 0,//カートのアイテム数
-  clicked = [],//クリックされたカートアイコンのインデックス
-  save_items = [],//ローカルストレージ保存用の配列
-  items = JSON.parse(localStorage.getItem("items"));//ローカルストレージの商品データ配列
-
-  // すでにカートに商品が入っている場合、カートアイコンのカウント表示とカートボタンをアクティブにする
-  if (items) {
-    var id;
-    for (var i = 0; i < items.length; i++) {
-      id = items[i].id;
-      save_items.push(items[i]);
-      clicked.push(id);
-      activate_btn(id);
-    }
-
-    if(items.length != 0){
-      cart_cnt_icon.parentNode.classList.remove('hidden');
-      cart_cnt_icon.innerHTML = cart_cnt;
-    }
-  }
-
-  // カートボタンを押した際の処理
-  cart_btns.forEach(function (cart_btn,index) {
-    cart_btn.addEventListener('click',function () {
-
-      // カートボタンがすでに押されているかの判定
-      if (clicked.indexOf(index) >= 0) {
-
-        for (var i = 0; i < clicked.length; i++) {
-          if(clicked[i] == index){
-            clicked.splice(i, 1);
-            save_items.splice(i, 1);
-          }
-        }
-
-        inactivate_btn(index);
-
-      }else if(clicked.indexOf(index) == -1){
-
-        var name = cart_btn.dataset.name,//商品の名前を取得
-        price = Number(cart_btn.dataset.price);//商品の値段を取得
-
-        clicked.push(index);
-        save_items.push({
-          id: index,
-          name: name,
-          price: price
-        });
-
-        activate_btn(index);
-
+  var cart_btns,
+    cart_cnt_icon,
+    clicked, //クリックされたカートアイコンのインデックス
+    save_items, //ローカルストレージ保存用の配列
+    cart_cnt, //カートのアイテム数
+    items;
+  function cart_active() {
+    cart_btns = document.querySelectorAll(".js_cart_btn"); //カートボタン
+    cart_cnt_icon = document.getElementById("js_cart_cnt");
+    clicked = [];
+    save_items = []; //カートの個数アイコン
+    cart_cnt = 0;
+    items = JSON.parse(localStorage.getItem("items")); //ローカルストレージの商品データ配列
+    // すでにカートに商品が入っている場合、カートアイコンのカウント表示とカートボタンをアクティブにする
+    if (items) {
+      var id;
+      for (var i = 0; i < items.length; i++) {
+        id = items[i].id;
+        save_items.push(items[i]);
+        clicked.push(id);
+        activate_btn(id);
       }
 
-      // ローカルストレージに商品データを保管
-      localStorage.setItem("items",JSON.stringify(save_items));
+      if (items.length != 0) {
+        cart_cnt_icon.parentNode.classList.remove("hidden");
+        cart_cnt_icon.innerHTML = cart_cnt;
+      }
+    }
 
+    // カートボタンを押した際の処理
+    cart_btns.forEach(function (cart_btn, index) {
+      cart_btn.addEventListener("click", function () {
+        // カートボタンがすでに押されているかの判定
+        if (clicked.indexOf(index) >= 0) {
+          for (var i = 0; i < clicked.length; i++) {
+            if (clicked[i] == index) {
+              clicked.splice(i, 1);
+              save_items.splice(i, 1);
+            }
+          }
+
+          inactivate_btn(index);
+        } else if (clicked.indexOf(index) == -1) {
+          var name = cart_btn.dataset.name, //商品の名前を取得
+            price = Number(cart_btn.dataset.price); //商品の値段を取得
+
+          clicked.push(index);
+          save_items.push({
+            id: index,
+            name: name,
+            price: price,
+          });
+
+          activate_btn(index);
+        }
+
+        // ローカルストレージに商品データを保管
+        localStorage.setItem("items", JSON.stringify(save_items));
+      });
     });
-  });
+  }
+
+  cart_active();
 
   function activate_btn(index) {
     cart_cnt++;
-    if( cart_cnt >= 1 ){
-      cart_cnt_icon.parentNode.classList.remove('hidden');
+    if (cart_cnt >= 1) {
+      cart_cnt_icon.parentNode.classList.remove("hidden");
     }
     cart_cnt_icon.innerHTML = cart_cnt;
-    cart_btns[index].classList.add('item_cart_btn_active');
+    cart_btns[index].classList.add("item_cart_btn_active");
   }
 
   function inactivate_btn(index) {
     cart_cnt--;
-    if(cart_cnt == 0){
-      cart_cnt_icon.parentNode.classList.add('hidden');
+    if (cart_cnt == 0) {
+      cart_cnt_icon.parentNode.classList.add("hidden");
     }
     cart_cnt_icon.innerHTML = cart_cnt;
-    cart_btns[index].classList.remove('item_cart_btn_active');
+    cart_btns[index].classList.remove("item_cart_btn_active");
   }
 
+  // 検索ボタンを押した際、検索結果を表示
+  $("#search-btn").on("click", () => {
+    var searchResult,
+      searchText = $("#search-text").val(), // 検索ボックスに入力された値
+      targetText,
+      hitNum;
+
+    // 検索結果を格納するための配列を用意
+    searchResult = [];
+
+    // 検索結果エリアの表示を空にする
+    $("#search-result__list").empty();
+    $(".search-result__hit-num").empty();
+
+    // 検索ボックスに値が入ってる場合
+    if (searchText != "") {
+      $(".item_wrapper li").each(function (i, element) {
+        console.log($(this).parent().children().html());
+        targetText = $(this).text();
+
+        // 検索対象となるリストに入力された文字列が存在するかどうかを判断
+        if (targetText.indexOf(searchText) != -1) {
+          // 存在する場合はそのリストのテキストを用意した配列に格納
+          searchResult.push($(this).html());
+        }
+      });
+
+      // 検索結果をページに出力
+      for (var i = 0; i < searchResult.length; i++) {
+        $("<div class='search-item'>")
+          .html(searchResult[i])
+          .appendTo("#search-result__list");
+      }
+
+      // ヒットの件数をページに出力
+      hitNum =
+        "<span>検索結果</span>：" + searchResult.length + "件見つかりました。";
+      $(".search-result__hit-num").append(hitNum);
+    }
+    $("item_cart_btn").on("click", () => {});
+  });
 };
-
-
-
-
